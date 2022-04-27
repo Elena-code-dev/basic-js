ea4141728ebb71bcc7603fa83a6a9708244c9d7a
Elena-code-dev <kev.76@inbox.ru>
2022-04-27 20:20:20 +0300

    add task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if(!isNaN(sampleActivity) &&
+    (parseFloat(sampleActivity) > 0 && parseFloat(sampleActivity) < 15) && (typeof sampleActivity == "string")) {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (Math.LN2 / HALF_LIFE_PERIOD));
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
