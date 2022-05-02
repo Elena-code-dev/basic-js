a75aafd6c02b68cb39787a10aa5cb14ad087c40a
Elena-code-dev <kev.76@inbox.ru>
2022-05-03 02:27:12 +0300

    feat: add task

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -12,9 +12,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
 function sortByHeight(arr) {
-  let filter = arr.filter((a,b) => a !== -1).sort()
+  let filter = arr.filter((a,b) => a !== -1).sort((a,b) => a-b)
   let res = arr.map((a,b,c) => a == -1 ? filter.splice(b,0,a) : a)
-  return filter;
+   return filter;
 }
 
 module.exports = {
